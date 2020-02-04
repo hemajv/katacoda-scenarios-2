00eb8072a0eac13f5160f599fd435fb0e4fefc10
4n4nd <4n4nd@redhat.com>
2020-02-04 21:07:59 +0000

    Deploy the demo metrics app before the scenario starts

--- deploy-prometheus-grafana/set-env.sh
+++ deploy-prometheus-grafana/set-env.sh
@@ -2,7 +2,9 @@
 launch.sh
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
 oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
-
+oc new-project metrics-demo
+oc new-app quay.io/4n4nd/metrics-demo-app:workshop
+oc expose svc/metrics-demo-app
 clear
 
 echo -e "Welcome to your interactive environment. OpenShift is configured and ready to use."
