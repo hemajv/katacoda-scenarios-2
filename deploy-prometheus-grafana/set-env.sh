f78230891361dd3f7770e8fb8c4a971db8736e84
4n4nd <4n4nd@redhat.com>
2020-02-10 20:55:08 +0000

    test prometheus.yml configmap

--- deploy-prometheus-grafana/set-env.sh
+++ deploy-prometheus-grafana/set-env.sh
@@ -1,12 +1,13 @@
 #Helps to address error: x509: certificate signed by unknown authority
 launch.sh
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
+mkdir -p /tmp/prometheus
 oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
 # deploy the demo metrics app
 oc new-project metrics-demo
 oc new-app quay.io/4n4nd/metrics-demo-app:workshop
 oc expose svc/metrics-demo-app
 clear
-
+cd /tmp/prometheus
 echo -e "Welcome to your interactive environment. OpenShift is configured and ready to use."
 echo -e "The demo metrics application is deployed at: \n http://$(oc get route metrics-demo-app -o jsonpath='{.spec.host}' -n metrics-demo)"
