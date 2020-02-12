00cbec1524348c90cfd8ac8c770fc5af4b469cea
4n4nd <4n4nd@redhat.com>
2020-02-12 04:27:10 +0000

    Add a 5 second sleep command before the build log is printed

--- pad-sarima/set-env.sh
+++ pad-sarima/set-env.sh
@@ -8,6 +8,8 @@ curl https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/2.
 
 oc process notebook-builder -p GIT_REPOSITORY_URL=https://github.com/hemajv/prometheus-anomaly-detection-workshop.git -p CONTEXT_DIR=source | oc apply -f - -n myproject
 oc process notebook-deployer -p NOTEBOOK_IMAGE=custom-notebook:latest -p NOTEBOOK_PASSWORD=secret | oc apply -f - -n myproject
-oc start-build custom-notebook
+sleep 5
 oc logs bc/custom-notebook -f
+echo -e "-------------------------------------------"
+echo -e "The environment should be ready in a few seconds"
 echo -e "The url to access the Jupyter Notebooks is: \n http://$(oc get route custom-notebook -o jsonpath='{.spec.host}' -n myproject)"
