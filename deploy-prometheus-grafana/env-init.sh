2ce10221f4bc79c3fe235d050bce1ba050f798d1
4n4nd <4n4nd@redhat.com>
2020-02-17 21:12:35 +0000

    Add PV templates

--- deploy-prometheus-grafana/env-init.sh
+++ deploy-prometheus-grafana/env-init.sh
@@ -1,3 +1,7 @@
 ssh root@host01 "docker pull centos/python-35-centos7:latest"
 ssh root@host01 "docker pull openshiftroadshow/parksmap-katacoda:1.0.0"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
+ssh root@host01 'oc adm policy add-cluster-role-to-group sudoer system:authenticated'
+ssh root@host01 "mkdir -p /data/pv-01"
+ssh root@host01 "chmod 0777 /data/pv-*"
+ssh root@host01 "oc create -f volumes.json"
