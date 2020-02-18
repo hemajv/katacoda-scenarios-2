99c4b7376c1a77f7e5d21ec9605b171adf43b6b8
4n4nd <4n4nd@redhat.com>
2020-02-18 20:28:05 +0000

    move generate metrics to background

--- prometheus-api-client/env-init.sh
+++ prometheus-api-client/env-init.sh
@@ -3,3 +3,4 @@ ssh root@host01 'oc adm policy add-cluster-role-to-group sudoer system:authentic
 ssh root@host01 "mkdir -p /data/pv-01 /data/pv-02 /data/pv-03 /data/pv-04 /data/pv-05 /data/pv-06 /data/pv-07 /data/pv-08 /data/pv-09 /data/pv-10"
 ssh root@host01 "chmod 0777 /data/pv-*; chcon -t svirt_sandbox_file_t /data/pv-*;"
 ssh root@host01 "oc create -f volumes.json"
+ssh root@host01 "oc process -f ~/generate-metrics.yaml | oc apply -n myproject -f - --as system:admin"
