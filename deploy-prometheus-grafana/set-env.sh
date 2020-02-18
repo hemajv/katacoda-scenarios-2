3d3c33bcad9c96c6c1b2be42811920c389aa0bbf
4n4nd <4n4nd@redhat.com>
2020-02-18 16:20:52 +0000

    Add OCP 3.11 playground to test for PVCs

--- deploy-prometheus-grafana/set-env.sh
+++ deploy-prometheus-grafana/set-env.sh
@@ -1,9 +1,10 @@
 #Helps to address error: x509: certificate signed by unknown authority
 launch.sh
 curl -LO https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > /dev/null 2>&1
-mkdir -p /tmp/prometheus
-oc login -u developer -p developer https://master:8443 --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
+oc login -u developer -p developer --certificate-authority=lets-encrypt-x3-cross-signed.pem --insecure-skip-tls-verify=true > /dev/null 2>&1
+
 # deploy the demo metrics app
+mkdir -p /tmp/prometheus
 oc new-project metrics-demo
 oc new-app quay.io/4n4nd/metrics-demo-app:workshop
 oc expose svc/metrics-demo-app
